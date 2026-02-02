1fc32471527c20689b91345ebc01d9b62f8bd64e
debayansaha <grindart004@gmail.com>
2026-02-03 02:20:06 +0530

    Build 0.2

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,19 @@
+require('dotenv').config();
+
 const express  = require('express');
+const cookie = require('cookie-parser');
+const cookieParser = require('cookie-parser');
+const auth = require('./routes/auth.route');
+
 const app = express();
+
+app.use(cookieParser())
+app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.send('Hello')
 });
+
+app.use('/api/auth',auth);
 
 module.exports = app;
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,15 +1,47 @@
-const userModel = require('../models/user.models');
+const { json } = require("express");
+const userModel = require("../models/user.models");
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
-async function registerUser(req,res){
+async function registerUser(req, res) {
+  const { firstName, lastName, email, password } = req.body;
 
-    const {firstName, lastName, email, password} = req.body;
+  //for checking if user exists with this email
+  const userExists = await userModel.findOne({ email });
 
-    //for checking if user exists with this email 
-    const userExists = await userModel.findOne({email});
+  if (userExists) {
+    return res.status(400).json({
+      message: "User already exists",
+    });
+  }
 
-    if(userExists){
-        return res.status(400).json({
-            message:"User already exists"
-        })
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await userModel.create({
+    firstName,
+    lastName,
+    email,
+    password: hashedPassword,
+  });
+
+  const token = jwt.sign({
+    id: user._id,
+  },process.env.JWT_TOKEN)
+
+  res.cookie("token",token);
+
+  res.status(201).json({
+    message: "User registered succesfully",
+    user:{
+        _id: user._id,
+        emai: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName
     }
+  })
+
+}
+
+module.exports={
+    registerUser
 }
