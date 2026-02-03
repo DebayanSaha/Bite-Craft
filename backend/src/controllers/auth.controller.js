ec4310b4ca66761a6d49fd35662559801a7ce1b7
debayansaha <grindart004@gmail.com>
2026-02-03 14:25:06 +0530

    Build 0.3

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 const { json } = require("express");
 const userModel = require("../models/user.models");
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 async function registerUser(req, res) {
   const { firstName, lastName, email, password } = req.body;
@@ -24,24 +24,67 @@ async function registerUser(req, res) {
     password: hashedPassword,
   });
 
-  const token = jwt.sign({
-    id: user._id,
-  },process.env.JWT_TOKEN)
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_TOKEN,
+  );
 
-  res.cookie("token",token);
+  res.cookie("token", token);
 
   res.status(201).json({
     message: "User registered succesfully",
-    user:{
-        _id: user._id,
-        emai: user.email,
-        firstName: user.firstName,
-        lastName: user.lastName
-    }
-  })
-
+    user: {
+      _id: user._id,
+      emai: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    },
+  });
 }
 
-module.exports={
-    registerUser
+async function loginUser(req, res) {
+  const { email, password } = req.body;
+
+  const userExists = await userModel.findOne({ email });
+
+  if (!userExists) {
+    return res.status(400).json({
+      message: " Invalid email or password",
+    });
+  }
+
+  const isPassword = await bcrypt.compare(password, userExists.password);
+  console.log(userExists , isPassword);
+  
+
+  if (!isPassword) {
+    return res.status(400).json({
+      message: "Invalid email or password",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: userExists._id,
+    },
+    process.env.JWT_TOKEN,
+  );
+
+  res.cookie("token", token);
+
+  res.status(200).json({
+    message: "Login succesfull",
+    user: {
+      _id: userExists._id,
+      emai: userExists.email,
+      firstName: userExists.firstName,
+      lastName: userExists.lastName,
+    },
+  });
 }
+
+module.exports = {
+  registerUser, loginUser
+};
