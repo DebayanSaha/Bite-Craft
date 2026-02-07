820053b74e504e40d357981db6cf542345e957b8
debayansaha <grindart004@gmail.com>
2026-02-07 22:33:01 +0530

    Build 0.7

--- backend/src/middlewares/auth.midlewares.js
+++ backend/src/middlewares/auth.midlewares.js
@@ -1,6 +1,22 @@
 const foodPartnerModel = require('../models/foodPartner.models')
 const jwt = require('jsonwebtoken')
 
-async function a(req,res,next){
+async function authFoodPartnerMiddleware(req,res,next){
 
+    const token = req.cookies.token;
+    if(!token){
+        return res.status(401).json({
+            message:"Please log-in first"
+        })
+    }
+
+    try {
+        const decoded = jwt.verify(token , process.env.JWT_TOKEN);
+    } catch (err) {
+        return res.status(401).json({
+            message: "Invalid Token"
+        })
+    }
 } 
+
+module.exports = authFoodPartnerMiddleware;
