66c23d216ec7a800b73860be621f12c26910be1a
debayansaha <grindart004@gmail.com>
2026-02-22 01:46:39 +0530

    Build 0.4

--- frontend/src/pages/hero/LandingPage.jsx
+++ frontend/src/pages/hero/LandingPage.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import 'remixicon/fonts/remixicon.css'
 
 const LandingPage = () => {
   const nav = useNavigate();
 
   return (
-    <div className="relative bg-black h-screen w-full flex items-center justify-center">
-      <div className="bg-[url('/images/background/387624.png')] bg-cover bg-center h-full w-full md:w-1/2 p-4">
-        <div className='absolute left-1/2 -translate-x-1/2 top-60 h-20 w-[90%] flex items-center justify-center'>
-          <h1 className=' mt-0 text-[18vw] font-[font4] font-medium uppercase'>Bite<span className='text-orange-800'>craft</span></h1>
+    <div className="relative bg-transparent w-full min-h-screen overflow-hidden">
+      <video autoPlay muted loop className='absolute w-full h-full object-cover -z-10' src="/videos/bg/0221.mp4" />
+      <div className='relative w-full h-30 p-3'>
+        <h1 className='font-[font9] md:text-5xl md:ml-3 text-3xl text-[#E8E4D7] md:tracking-[2px] tracking-[1px] mt-2 md:mt-4'>Bitecraft.</h1>
+        <div className='absolute right-0 top-5 md:w-70 w-45 font-[font2] text-[#E8E4D7] flex md:gap-10 gap-5'>
+          <h1 onClick={()=>nav('/partner/login')} className='cursor-pointer font-[font5] md:text-xl md:mt-4 mt-2'>Partner with us</h1>
+          <button onClick={()=>nav('/user/login')} className='cursor-pointer bg-yellow-500 font-[font5] font-semibold md:px-6 px-4 md:py-3 py-2 md:text-2xl text-zinc-800 rounded-2xl '>Sign in</button>
         </div>
-        <button onClick={()=>nav('/user/register')} className='absolute h-10 w-36 bottom-70 left-1/2 -translate-x-1/2 font-[font1] text-[15px] text-red-900 bg-white/20 border border-white/10 backdrop-blur-[2px] rounded-full shadow-lg shadow-amber-600 active:scale-95 transition-transform duration-150'>Get Started</button>
+      </div>
+      <div className='absolute w-full h-45 md:h-80 bottom-0 p-4 md:px-8'>
+        <h1 className='font-[font5] text-[#E8E4D7] text-7xl md:text-9xl '>"Your Feed</h1>
+        <h1 className='font-[font6] text-[#E8E4D7] text-5xl md:text-8xl'>Just Got <span className='text-[#f4ca59]'>Delicious</span>."</h1>
       </div>
     </div>
   )
--- frontend/src/pages/user/UserLogin.jsx
+++ frontend/src/pages/user/UserLogin.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const UserLogin = () => {
+  const nav = useNavigate();
+  const [formData, setformData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleChange =(e)=> {
+    setformData({
+      ...formData,
+      [e.target.name]: e.target.value, 
+    })
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log("Login Data:", formData);
+  }
+
   return (
-    <div>UserLogin</div>
+    <div className="relative min-h-screen bg-[#f3f3f3] flex justify-center p-4">
+      <div className='absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 border-2 border-black h-110 w-80 rounded-3xl py-2 px-4'>
+        <div className='absolute right-2 bg-red-300 h-8 w-8 rounded-full flex items-center justify-center text-2xl'>
+          <i className="ri-arrow-left-s-line"></i>
+        </div>
+        <h1 className="text-4xl font-[font8] text-black mt-8">
+          Login
+        </h1>
+
+        <p className="text-gray-600 mb-6">
+          or{" "}
+          <span onClick={()=>nav('/partner/register')} className="text-orange-500 text-2xl font-[font7] cursor-pointer">
+            create an account
+          </span>
+        </p>
+      </div>
+      
+    </div>
   )
 }
 
