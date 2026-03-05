9458f3c9c5376b35692385675a54c0ede1e12eea
debayansaha <grindart004@gmail.com>
2026-03-05 23:38:39 +0530

    Build 2.9

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='absolute right-0 z-100 h-full w-20 bg-black'></div>
+    <div className='absolute right-0 z-100 h-full w-15  flex items-center justify-center'>
+        <i className="ri-shopping-cart-2-fill absolute top-5 text-3xl text-red-500"></i>
+        {/* <i className="ri-poker-hearts-fill text-3xl text-orange-500"></i> */}
+    </div>
   )
 }
 
