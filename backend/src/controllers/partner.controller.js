bfcf2b4aa137258f874c7f021ad03ab358e301f6
debayansaha <grindart004@gmail.com>
2026-03-06 23:09:12 +0530

    Build 3.3

--- backend/src/controllers/partner.controller.js
+++ backend/src/controllers/partner.controller.js
@@ -1,16 +1,20 @@
 const foodPartnerModel = require('../models/foodPartner.models');
-
+const foodModel = require('../models/food.models')
 async function getPartnerById(req,res){
     const partnerId = req.params.id;
 
     const partner = await foodPartnerModel.findById(partnerId);
+    const foodItemsByPartner = await foodModel.find({partner:partnerId});
 
     if(!partner){
         return res.status(400).json({message: "Food Partner not found"});
     }
     res.status(200).json({
         message:"Food Partner found successfully",
-        partner
+        partner:{
+            ...partner.toObject(), 
+            foodItems: foodItemsByPartner
+        }
     });
 }
 module.exports={
